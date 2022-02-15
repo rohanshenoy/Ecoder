8b0157bfb701056c98bdf16053c5762f15624982
rohanshenoy <42732515+rohanshenoy@users.noreply.github.com>
2022-02-15 12:59:41 -0800

    reducing batch sizes

--- ae_emd_cnn.py
+++ ae_emd_cnn.py
@@ -179,12 +179,12 @@ class ae_EMD_CNN:
                      ModelCheckpoint('/ecoderemdvol/22EMD/ae/ae_emd_models/'+str(num_filt)+str(kernel_size)+str(num_dens_neurons)+str(num_dens_layers)+str(num_conv_2d)+str(num_epochs)+Loss+'last.h5', monitor='val_loss', verbose=1, save_last_only=True),
                     ]
         
-        opt = tf.keras.optimizers.Adam(learning_rate=4e-3)
+        #opt = tf.keras.optimizers.Adam(learning_rate=4e-3)
 
-        sym_model.compile(optimizer=opt, loss=Loss, metrics=['mse', 'mae', 'mape', 'msle'])
+        sym_model.compile(optimizer='adam', loss=Loss, metrics=['mse', 'mae', 'mape', 'msle'])
         history = sym_model.fit((X1_train, X2_train), y_train, 
                             validation_data=((X1_val, X2_val), y_val),
-                            epochs=num_epochs, verbose=1, batch_size=128, callbacks=callbacks)
+                            epochs=num_epochs, verbose=1, batch_size=32, callbacks=callbacks)
         
         #Making directory for graphs
 
