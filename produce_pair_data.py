0f97dba468e65f6dd4b451b081d7c9d7289f249a
rohanshenoy <42732515+rohanshenoy@users.noreply.github.com>
2022-03-24 13:51:40 -0700

    splitting jobs

--- produce_pair_data.py
+++ produce_pair_data.py
@@ -4,16 +4,25 @@ import itertools
 from utils.metrics import emd
 from utils.metrics import hexMetric
 
-def main():
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-c',type=int, default=0, dest="cut",help="split jobs")
+
+def main(args):
     
-    data_dir='/ecoderemdvol/HGCal22Data_signal_driven_ttbar_v11/nElinks_5/data.csv'
+    cut_int=args.cut
+    
+    data_dir='/ecoderemdvol/HGCal22Data_signal_driven_ttbar_v11/nElinks_5/5Elinks_data.csv'
     CALQ_COLS = ['CALQ_%i'%c for c in range(0, 48)]
 
-    data =pd.read_csv(data_dir, dtype=np.float64, header=0, usecols=[*range(0,48)], names=CALQ_COLS, nrows=20000)
+    data =pd.read_csv(data_dir, dtype=np.float64, header=0, usecols=[*range(0,48)], names=CALQ_COLS, nrows=1000)
     data = data[CALQ_COLS].astype('float64')
     
     calQ = data.values
-    test_indices = range(0, len(calQ))
+    start_ind = int((cut_int/100)*len(calQ))
+    end_ind   = int(((cut_int+1)/100)*len(calQ))
+    test_indices = range(start_ind, end_ind)
 
     idx1_test = np.array([i for i,j in itertools.product(test_indices,test_indices)])
     idx2_test = np.array([j for i,j in itertools.product(test_indices,test_indices)])
@@ -28,7 +37,8 @@ def main():
             thresh_j=np.append(thresh_j,j)
     thresh_data=pd.DataFrame([thresh_i,thresh_j])
     thresh_data=thresh_data.T
-    thresh_data.to_csv('/ecoderemdvol/HGCal22Data_signal_driven_ttbar_v11/thresh_pair_indices.csv',index=False)
+    thresh_data.to_csv('/ecoderemdvol/HGCal22Data_signal_driven_ttbar_v11/pair/thresh_pair_'+str(cut_int)+'_indices.csv',index=False)
 
 if __name__ == '__main__':
-    main()
+    args = parser.parse_args()
+    main(args)
