c7f21afbdcbc485ba936bb2e9b24c0652434e464
rohanshenoy <42732515+rohanshenoy@users.noreply.github.com>
2022-07-30 17:22:17 -0700

    compare split eta trainings

--- comp_eta_overlay.py
+++ comp_eta_overlay.py
@@ -13,13 +13,13 @@ import os
 import pandas as pd
 
 def load_data(inputFile):
-    data=pd.read_csv(inputFile,usecols=[*range(0,48)],nrows=800000)
+    data=pd.read_csv(inputFile,usecols=[*range(0,48)],nrows=800)
     data_values=data.values
             
     return data_values
 
 def load_phy(inputFile):
-    data=pd.read_csv(inputFile,usecols=[*range(48,49)],nrows=800000)
+    data=pd.read_csv(inputFile,usecols=[*range(48,49)],nrows=80)
     data_values=data.values
             
     return data_values
@@ -44,63 +44,61 @@ egun_emd_output   = '/ecoderemdvol/EleGun/AE/comp_3/8x8_c8_S2_ae_mse/verify_deco
 
 """
 
-phys_ttbar = load_phy('/ecoderemdvol/22AE/aps_plots/8x8_c8_S2_tele/verify_input_calQ.csv')
-phys_egun  = load_phy('/ecoderemdvol/EleGun/AE/comp_3/8x8_c8_S2_tele/verify_input_calQ.csv')
-
-ttbar_tele_input_Q  = load_data('/ecoderemdvol/22AE/aps_plots/8x8_c8_S2_tele/verify_input_calQ.csv')
-
-ttbar_tele_output_Q = load_data('/ecoderemdvol/22AE/aps_plots/8x8_c8_S2_tele/verify_decoded_calQ.csv')
-
-ttbar_emd_input_Q   = load_data('/ecoderemdvol/22AE/aps_plots/8x8_c8_S2_ae_mse/verify_input_calQ.csv')
-
-ttbar_emd_output_Q  = load_data('/ecoderemdvol/22AE/aps_plots/8x8_c8_S2_ae_mse/verify_decoded_calQ.csv')
+eta_1_phys = load_phy('/ecoderemdvol/EleGun/EPGun-PU200/AE/comp_ae_mse_eta_1_tele/8x8_c8_S2_ae_mse_eta_1/verify_input_calQ.csv')
+eta_2_phys = load_phy('/ecoderemdvol/EleGun/EPGun-PU200/AE/comp_ae_mse_eta_2_tele/8x8_c8_S2_ae_mse_eta_2/verify_input_calQ.csv')
+full_eta_phys = load_phy('/ecoderemdvol/EleGun/EPGun-PU200/AE/comp_ae_mse_full_eta_tele_2/8x8_c8_S2_ae_mse_full_eta/verify_input_calQ.csv')
 
 
-egun_tele_input_Q   = load_data('/ecoderemdvol/EleGun/AE/comp_3/8x8_c8_S2_tele/verify_input_calQ.csv')
+eta_1_input_Q = load_data('/ecoderemdvol/EleGun/EPGun-PU200/AE/comp_ae_mse_eta_1_tele/8x8_c8_S2_ae_mse_eta_1/verify_input_calQ.csv')
+eta_2_input_Q = load_data('/ecoderemdvol/EleGun/EPGun-PU200/AE/comp_ae_mse_eta_2_tele/8x8_c8_S2_ae_mse_eta_2/verify_input_calQ.csv')
+full_eta_input_Q = load_data('/ecoderemdvol/EleGun/EPGun-PU200/AE/comp_ae_mse_full_eta_tele_2/8x8_c8_S2_ae_mse_full_eta/verify_input_calQ.csv')
 
-egun_tele_output_Q  = load_data('/ecoderemdvol/EleGun/AE/comp_3/8x8_c8_S2_tele/verify_decoded_calQ.csv')
+eta_1_output_Q = load_data('/ecoderemdvol/EleGun/EPGun-PU200/AE/comp_ae_mse_eta_1_tele/8x8_c8_S2_ae_mse_eta_1/verify_decoded_calQ.csv')
+eta_2_output_Q = load_data('/ecoderemdvol/EleGun/EPGun-PU200/AE/comp_ae_mse_eta_2_tele/8x8_c8_S2_ae_mse_eta_2/verify_decoded_calQ.csv')
+full_eta_output_Q = load_data('/ecoderemdvol/EleGun/EPGun-PU200/AE/comp_ae_mse_full_eta_tele_2/8x8_c8_S2_ae_mse_full_eta/verify_decoded_calQ.csv')
 
-egun_emd_input_Q    = load_data('/ecoderemdvol/EleGun/AE/comp_3/8x8_c8_S2_ae_mse/verify_input_calQ.csv')
-
-egun_emd_output_Q   = load_data('/ecoderemdvol/EleGun/AE/comp_3/8x8_c8_S2_ae_mse/verify_decoded_calQ.csv')
     
 #Calculate offset for better presentation
-indices_ttbar = range(0,(len(phys_ttbar)))
-    
-indices_ttbar = range(0,(len(ttbar_tele_input_Q)))
+indices_full = range(0,(len(full_eta_phys)))
 
-eta_ttbar=[]
+eta_full=[]
                   
-for i in indices_ttbar:
-    eta_ttbar=np.append(eta_ttbar,phys_ttbar[i][0])
+for i in indices_full:
+    eta_full=np.append(eta_full,full_eta_phys[i][0])
     
-offset=0.75*((np.max(eta_ttbar)-np.min(eta_ttbar))/10)/4
+offset=0.75*((np.max(eta_full)-np.min(eta_full))/10)/3
         
-indices_egun = range(0,(len(egun_tele_input_Q)))
+indices_eta_1 = range(0,(len(eta_1_phys)))
     
-eta_egun=[]
+eta_1=[]
                   
-for i in indices_egun:
-    eta_egun=np.append(eta_egun,phys_egun[i][0])
+for i in indices_eta_1:
+    eta_1=np.append(eta_1,eta_1_phys[i][0])
     
-emd_values_ttbar_tele = np.array([emd(ttbar_tele_input_Q[i],ttbar_tele_output_Q[j]) for i, j in zip(indices_ttbar,indices_ttbar)]) 
-emd_values_ttbar_emd  = np.array([emd(ttbar_emd_input_Q[i],ttbar_emd_output_Q[j]) for i, j in zip(indices_ttbar,indices_ttbar)]) 
-emd_values_egun_tele  = np.array([emd(egun_tele_input_Q[i],egun_tele_output_Q[j]) for i, j in zip(indices_egun,indices_egun)])     
-emd_values_egun_emd   = np.array([emd(egun_emd_input_Q[i],egun_emd_output_Q[j]) for i, j in zip(indices_egun,indices_egun)])
+indices_eta_2 = range(0,(len(eta_2_phys)))
+    
+eta_2=[]
+                  
+for i in indices_eta_2:
+    eta_2=np.append(eta_2,eta_2_phys[i][0])
+    
+emd_values_full = np.array([emd(full_eta_input_Q[i],full_eta_output_Q[j]) for i, j in zip(indices_full,indices_full)])
 
-emd_values_arr=[emd_values_ttbar_tele,emd_values_ttbar_emd,emd_values_egun_tele,emd_values_egun_emd]
-labels = ['ttbar_tele','ttbar_emd','egun_tele','egun_emd']
+emd_values_1 = np.array([emd(eta_1_input_Q[i],eta_1_output_Q[j]) for i, j in zip(indices_eta_1,indices_eta_1)])
+
+emd_values_2 = np.array([emd(eta_2_input_Q[i],eta_2_output_Q[j]) for i, j in zip(indices_eta_2,indices_eta_2)])
+
+emd_values_arr=[emd_values_full,emd_values_1,emd_values_2]
+eta_arr= [eta_full,eta_1,eta_2]
+labels = ['full_eta','eta_1','eta_2']
+offset = [0,-0.04,0.04]
 
 fig,ax=plt.subplots()
 plt.figure(figsize=(6,4))
 
 for iteration,emd in enumerate(emd_values_arr):
     
-    x=[]
-    if iteration == 0 or iteration == 1:
-        x = eta_ttbar
-    else:
-        x = eta_egun
+    x=eta_arr[iteration]
     
     y = emd
     
@@ -118,15 +116,13 @@ for iteration,emd in enumerate(emd_values_arr):
     bin_edges = median_result.bin_edges
     bin_centers = (bin_edges[:-1] + bin_edges[1:])/2.
     
-    off = 0
-    if iteration != 3:
-        off = offset*iteration
+    off = offset[iteration]
     plt.errorbar(x=bin_centers+off, y=median, yerr=[loe,hie], label=labels[iteration])
     
 plt.legend(loc='upper right')
 plt.xlabel(r'$\eta$')
 plt.ylabel('EMD')
-plt.savefig('/ecoderemdvol/ttbar_egun_ae_comp_eta_EMD.pdf',dpi=600)
+plt.savefig('comp_split_eta_EMD.pdf',dpi=300)
     
     
 
