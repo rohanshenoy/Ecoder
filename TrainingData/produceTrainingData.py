db2f6d4977cd47ee7ab5f408903a0199714b2061
rohanshenoy <42732515+rohanshenoy@users.noreply.github.com>
2022-08-19 14:18:11 -0700

    making gen level cuts on event with coffea

--- TrainingData/produceTrainingData.py
+++ TrainingData/produceTrainingData.py
@@ -3,42 +3,66 @@ import uproot
 import numpy as np
 import pandas as pd
 import awkward as ak
+from coffea.nanoevents import NanoEventsFactory, BaseSchema
 
 def loadTrainingData(inputRoot,
                      rootFileTDirectory='FloatingpointThreshold0DummyHistomaxGenmatchGenclustersntuple',
                      outputFileName='CALQ.csv',
                      N_eLinks=5,
-                     abs_eta_min=1.5,
-                     abs_eta_max=2.0,
+                     gen_pt_max = 200,
+                     abs_eta_min=1.6,
+                     abs_eta_max=3.0,
                      useADC=False):
     current_dir=os.getcwd()
     mergeTrainingData = pd.DataFrame()
     if os.path.isdir(inputRoot):
-        
         for infile in os.listdir(inputRoot):
             if os.path.isdir(inputRoot+infile): continue
             inputRootFile = os.path.join(inputRoot,infile)
             
-            _tree = uproot.open(inputRootFile)[f'{rootFileTDirectory}/HGCalTriggerNtuple']
+            eventFile = uproot.open(inputRootFile)
+            
+            events = NanoEventsFactory.from_root(
+            eventFile,
+            treepath=rootFileTDirectory+'/HGCalTriggerNtuple',
+            schemaclass=BaseSchema).events()
+            
+            gen_pt = events.gen_pt
+            gen_eta = events.gen_eta
+            
+            #make gen_pt and gen_eta cuts, gen eta cuts on electrons in positive eta, positron is also in same event
+            
+            pt_mask = (gen_pt <=gen_pt_max)[:,1]
+            eta_min_mask = (gen_eta >=abs_eta_min)[:,1]
+            eta_max_mask = (gen_eta <= abs_eta_max)[:,1]
 
-            hasSimEnergy = 'tc_simenergy' in _tree
-            if hasSimEnergy:
-                arrays = _tree.arrays(['tc_zside','tc_layer','tc_waferu','tc_waferv','tc_cellu','tc_cellv','tc_data','tc_eta','tc_simenergy','gen_pt'])
-            else:
-                arrays = _tree.arrays(['tc_zside','tc_layer','tc_waferu','tc_waferv','tc_cellu','tc_cellv','tc_data','tc_eta','gen_pt'])
-
-            select_eLinks = {5 : (arrays[b'tc_layer']==9),
-                             4 : (arrays[b'tc_layer']==7) | (arrays[b'tc_layer']==11),
-                             3 : (arrays[b'tc_layer']==13),
-                             2 : (arrays[b'tc_layer']<7) | (arrays[b'tc_layer']>13),
-                             -1 : (arrays[b'tc_layer']>0)}
+            mask = ak.Array(np.logical_and(np.asarray(pt_mask),np.asarray(eta_min_mask),np.asarray(eta_max_mask)))
+            
+            tc_mask_data = ak.Array(
+                {
+                    'tc_zside': events['tc_zside'][mask],
+                    'tc_layer': events['tc_layer'][mask],
+                    'tc_waferu': events['tc_waferu'][mask],
+                    'tc_waferv': events['tc_waferv'][mask],
+                    'tc_cellu': events['tc_cellu'][mask],
+                    'tc_cellv': events['tc_cellv'][mask],
+                    'tc_data': events['tc_data'][mask],
+                    'tc_simenergy': events['tc_simenergy'][mask],
+                    'tc_eta': events['tc_eta'][mask]
+                }
+            )
+            
+            select_eLinks = {5 : (tc_mask_data[b'tc_layer']==9),
+                             4 : (tc_mask_data[b'tc_layer']==7) | (tc_mask_data[b'tc_layer']==11),
+                             3 : (tc_mask_data[b'tc_layer']==13),
+                             2 : (tc_mask_data[b'tc_layer']<7) | (tc_mask_data[b'tc_layer']>13),
+                             -1 : (tc_mask_data[b'tc_layer']>0)}
 
             assert N_eLinks in select_eLinks
+            
+            df = ak.to_pandas(tc_mask_data[select_eLinks[N_eLinks]])
 
-            df = ak.to_pandas(arrays[select_eLinks[N_eLinks]])
-            df = df[df.gen_pt < 35]
-
-            dfRemap = pd.read_csv('tcRemap.csv')
+            dfRemap = pd.read_csv('/ecoderemdvol/tcRemap.csv')
             df = df.reset_index().merge(dfRemap)
 
             df['ADCT'] = (df.tc_data* ((1./np.cosh(df.tc_eta))*2**12).astype(int)/2**12).astype(int)
@@ -54,17 +78,13 @@ def loadTrainingData(inputRoot,
 
             dfTrainData[['entry','zside','layer','waferu','waferv']] = df.groupby(['WaferEntryIdx'])[['entry','tc_zside','tc_layer','tc_waferu','tc_waferv']].mean()
 
-            if hasSimEnergy:
-                dfTrainData['simenergy'] = df.groupby(['WaferEntryIdx'])[['tc_simenergy']].sum()
+            dfTrainData['simenergy'] = df.groupby(['WaferEntryIdx'])[['tc_simenergy']].sum()
 
             #Mapping wafer_u,v to physical coordinates
-            dfEtaPhi=pd.read_csv('WaferEtaPhiMap.csv')
+            dfEtaPhi=pd.read_csv('/ecoderemdvol/WaferEtaPhiMap.csv')
             dfTrainData=dfTrainData.merge(dfEtaPhi, on=['layer','waferu','waferv'])
             dfTrainData.reset_index(drop=True,inplace=True)
             mergeTrainingData=pd.concat([mergeTrainingData,dfTrainData])
-    
-    mergeTrainingData = mergeTrainingData[abs(mergeTrainingData.tc_eta)>abs_eta_min]
-    mergeTrainingData = mergeTrainingData[abs(mergeTrainingData.tc_eta)<abs_eta_max]
       
     if '.csv' in outputFileName:
         mergeTrainingData.to_csv(outputFileName,index=False)
@@ -84,8 +104,9 @@ if __name__=="__main__":
     parser.add_argument('-N',dest='N_eLinks',type=int,default=5,help='Number of eRx to select')
     parser.add_argument('--ADC',dest='useADC',default=False,action='store_true',help='Use ADC rather than transverse ADC')
     parser.add_argument('-o','--output',dest='outputFileName',default='CALQ.csv',help='Output file name (either a .csv or .pkl file name)')
-    parser.add_argument('--eta_min', dest='abs_eta_min', type=float, default=1.50, help='minimum wafer_eta')
-    parser.add_argument('--eta_max', dest='abs_eta_max', type=float, default=2.00, help='maximum wafer_eta')
+    parser.add_argument('--pt_max',  dest='gen_pt_max',  type=float, default=200, help='maximum gen_pt')
+    parser.add_argument('--eta_min', dest='abs_eta_min', type=float, default=1.6, help='minimum gen_eta')
+    parser.add_argument('--eta_max', dest='abs_eta_max', type=float, default=3.0, help='maximum gen_eta')
     
     args = parser.parse_args()
 
@@ -93,6 +114,7 @@ if __name__=="__main__":
                           rootFileTDirectory = args.rootFileTDirectory,
                           outputFileName = args.outputFileName,
                           N_eLinks = args.N_eLinks,
+                          gen_pt_max = args.gen_pt_max,
                           abs_eta_min = args.abs_eta_min,
                           abs_eta_max = args.abs_eta_max,
                           useADC = args.useADC)
